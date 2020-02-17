25e228e94ca5a62d2c2c8c322b8abc3012054c6f
DIR\mihai.pitu <mihai.pitu@accenture.com>
2020-02-17 14:41:01 +0200

    [Editor] Added Tutorial Model Class. A method to store the tutorials TBA. Not sure what to choose.

---
+++ EditorApp/EditorApp/Models/Tutorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditorApp.Models
+{
+    public class Tutorial
+    {
+        private int id;
+        private string title;
+        private string description;
+        private string task;
+        private string answer;
+
+        public int Id { get => id; set => id = value; }
+        public string Title { get => title; set => title = value; }
+        public string Description { get => description; set => description = value; }
+        public string Task { get => task; set => task = value; }
+        public string Answer { get => answer; set => answer = value; }
+    }
+}
